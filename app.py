f53283fc5e79cb0368767d36d9fe37444cb30953
Rick Hsu <56618553+as183789043@users.noreply.github.com>
2024-01-14 17:32:48 +0800

    First commit to check get camera permission

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit  as st
+import av
+from streamlit_webrtc import webrtc_streamer
+
+def webcam_input():
+    st.header("Webcam Live Feed")
+    WIDTH = st.sidebar.select_slider('QUALITY (May reduce the speed)', list(range(150, 501, 50)))
+    width = WIDTH
+
+
+
+    def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+        image = frame.to_ndarray(format="bgr24")
+
+        orig_h, orig_w = image.shape[0:2]
+
+        # cv2.resize used in a forked thread may cause memory leaks
+        input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
+
+        transferred = style_transfer(input, model)
+
+        result = Image.fromarray((transferred * 255).astype(np.uint8))
+        image = np.asarray(result.resize((orig_w, orig_h)))
+        return av.VideoFrame.from_ndarray(image, format="bgr24")
+    
+    ctx = webrtc_streamer(
+    key="neural-style-transfer",
+    video_frame_callback=video_frame_callback,
+)
+
+
+st.sidebar.title("Get permission about Web Camera ")
+
+webcam_input()
