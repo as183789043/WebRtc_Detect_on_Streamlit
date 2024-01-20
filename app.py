5f7926cd73d49d63917447cd2cf7e3c06752b3e8
Rick Hsu <56618553+as183789043@users.noreply.github.com>
2024-01-21 00:56:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit  as st
 import av
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
 from turn import get_ice_servers
+from ultralytics import YOLO
+
+# Load the YOLOv8 model
+model = YOLO('yolov8n.pt')
 
 def webcam_input():
     st.header("Webcam Live Feed")
@@ -18,18 +22,26 @@ def webcam_input():
         # cv2.resize used in a forked thread may cause memory leaks
         input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
 
-        transferred = style_transfer(input, model)
+        results = model(frame)
+        annotated_frame = results[0].plot()
 
-        result = Image.fromarray((transferred * 255).astype(np.uint8))
+
+        result = Image.fromarray((annotated_frame * 255).astype(np.uint8))
         image = np.asarray(result.resize((orig_w, orig_h)))
+        
         return av.VideoFrame.from_ndarray(image, format="bgr24")
     
-    ctx = webrtc_streamer(
-        key="neural-style-transfer",
-        video_frame_callback=video_frame_callback,
-        rtc_configuration={"iceServers": get_ice_servers()},
-        media_stream_constraints={"video": True, "audio": False},
-    )
+    webrtc_streamer(
+    key="object-detection",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration={
+        "iceServers": get_ice_servers(),
+        "iceTransportPolicy": "relay",
+    },
+    video_frame_callback=video_frame_callback,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+)
 
 
 st.sidebar.title("Get permission about Web Camera ")
