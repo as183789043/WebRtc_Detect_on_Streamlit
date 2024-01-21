c97fbd8605dc2916e0a7a39493f4945d2f53cd65
Rick Hsu <56618553+as183789043@users.noreply.github.com>
2024-01-21 14:12:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,49 +1,29 @@
-import streamlit  as st
-import av
-from streamlit_webrtc import WebRtcMode, webrtc_streamer
-from turn import get_ice_servers
+
+import cv2
+import streamlit as st
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer,WebRtcMode,RTCConfiguration
 from ultralytics import YOLO
+import av 
 
-# Load the YOLOv8 model
-model = YOLO('yolov8n.pt')
-
-def webcam_input():
-    st.header("Webcam Live Feed")
-    WIDTH = st.sidebar.select_slider('QUALITY (May reduce the speed)', list(range(150, 501, 50)))
-    width = WIDTH
-
-
-
-    def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-        image = frame.to_ndarray(format="bgr24")
-
-        orig_h, orig_w = image.shape[0:2]
-
-        # cv2.resize used in a forked thread may cause memory leaks
-        input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
-
-        results = model(frame)
-        annotated_frame = results[0].plot()
-
-
-        result = Image.fromarray((annotated_frame * 255).astype(np.uint8))
-        image = np.asarray(result.resize((orig_w, orig_h)))
-        
-        return av.VideoFrame.from_ndarray(image, format="bgr24")
-    
-    webrtc_streamer(
-    key="object-detection",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration={
-        "iceServers": get_ice_servers(),
-        "iceTransportPolicy": "relay",
-    },
-    video_frame_callback=video_frame_callback,
-    media_stream_constraints={"video": True, "audio": False},
-    async_processing=True,
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
+model=YOLO("yolov8n.pt")
 
-st.sidebar.title("Get permission about Web Camera ")
 
-webcam_input()
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    results = model(img)
+    annotated_frame = results[0].plot()
+
+    return av.VideoFrame.from_ndarray(annotated_frame, format="bgr24")
+
+
+ctx = webrtc_streamer(key="example",      
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_frame_callback=video_frame_callback,
+        async_processing=True,)
+
