5005238febef94f4928621bdd8315a4bb9b12f98
Rick Hsu <56618553+as183789043@users.noreply.github.com>
2024-01-23 00:43:11 +0800

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,6 +1,15 @@
 import av
-def video_frame_callback(frame):
+import streamlit as st
+from ultralytics import YOLO
+
+@st.cache_resource
+def load_model(model_name):
+    return YOLO(model_name)
+
+
+def video_frame_callback(frame,model_name):
     img = frame.to_ndarray(format="bgr24")
+    model = load_model(model_name)
     results = model(img)
     annotated_frame = results[0].plot()
 
