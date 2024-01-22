68329f7674d2f7fb51765b1ee5072d6b5223d539
Rick Hsu <56618553+as183789043@users.noreply.github.com>
2024-01-23 01:51:15 +0800

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,12 +1,24 @@
 import av
 import streamlit as st
 from ultralytics import YOLO
-
+import numpy as np
+from PIL import Image
+#Model
 @st.cache_resource
 def load_model(model_name):
     return YOLO(model_name)
 
+#Image type to numpy
+def image_process(upload_image,model_name):
+    img= Image.open(upload_image)
+    np_img = np.array(img) #nparray
+    model = load_model(model_name)
+    results = model(np_img)
+    for result in results:
+        return result.plot()
 
+
+#WebRTC Process
 def video_frame_callback(frame,model_name):
     img = frame.to_ndarray(format="bgr24")
     model = load_model(model_name)
